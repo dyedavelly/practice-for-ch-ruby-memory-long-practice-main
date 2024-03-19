aec96d2abfb4389e18dafc4600eb9aeac2a1a737
katie leong <katieleong@yahoo.com>
2024-03-19 13:14:22 -0700

    worked on Board class

--- practice-for-ch-ruby-memory-long-practice-main/board.rb
+++ practice-for-ch-ruby-memory-long-practice-main/board.rb
@@ -1,13 +1,19 @@
 require 'byebug'
+
 class Board
     def initialize(n=4)
         @cards = Array.new(n){Array.new(n)}
         @size = n*n
     end
     def populate
-        random_card_pairs = ('a'..'z').to_a.sample
-        # random array of 16cards
-        #card pairs will be random
+        random_chars = []
+        until random_chars.length == @size / 2
+            char = ('a'..'z').to_a.sample
+            if !random_chars.include?(char) 
+                random_chars << char
+            end
+        end
+        random_pairs = (random_chars + random_chars).shuffle!
 
         @cards.each do |subarray|
             subarray.each_with_index do |spot, i|
