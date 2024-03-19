643392cffc353caa2ddc2d50aa00839c556940f7
Dharani <dharani1414@gmail.com>
2024-03-19 13:45:39 -0700

    working on board#methods

--- practice-for-ch-ruby-memory-long-practice-main/board.rb
+++ practice-for-ch-ruby-memory-long-practice-main/board.rb
@@ -2,6 +2,7 @@ require 'byebug'
 
 class Board
     def initialize(n=4)
+        @n = n
         @cards = Array.new(n){Array.new(n)}
         @size = n*n
     end
@@ -16,11 +17,21 @@ class Board
         random_pairs = (random_chars + random_chars).shuffle!
 
         @cards.each do |subarray|
-            subarray.each_with_index do |spot, i|
-                #debugger
-                subarray[i] = 
+            subarray.each_with_index do |ele, i|
+                subarray[i] = random_pairs.pop
             end
         end
         @cards   
+    end
+     
+    def render
+        print "  "
+        @n.times { |ele| print ele.to_s + " " }
+        puts 
+        @cards.each_with_index do |subarray, row|
+            print row
+            print " "
+            p subarray.join(" ")
+        end
     end
 end
